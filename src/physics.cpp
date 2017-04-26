820257428698fa6da6d887e9fc6eb5b398deb5e8
Kibium <toniferrari87@gmail.com>
2017-04-26 10:00:00 +0200

    Translate

--- src/physics.cpp
+++ src/physics.cpp
@@ -1,7 +1,18 @@
+#include <glm\gtc\matrix_transform.hpp>
 #include <imgui\imgui.h>
 #include <imgui\imgui_impl_glfw_gl3.h>
+#include <glm\mat4x4.hpp>
 
+namespace Cube {
+	extern void setupCube();
+	extern void cleanupCube();
+	extern void updateCube(const glm::mat4& transform);
+	extern void drawCube();
+}
+extern glm::vec3 randPos;
+extern float x, y, z;
 bool show_test_window = false;
+
 void GUI() {
 	{	//FrameRate
 		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
@@ -10,7 +21,7 @@ void GUI() {
 	}
 
 	// ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
-	if(show_test_window) {
+	if (show_test_window) {
 		ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
 		ImGui::ShowTestWindow(&show_test_window);
 	}
@@ -21,7 +32,16 @@ void PhysicsInit() {
 }
 void PhysicsUpdate(float dt) {
 	//TODO
+	randPos = glm::vec3(x, y, z);
+	y = 4;
+	z = 0;
+	x = 0;
+	glm::mat4 position(1.f);
+	glm::translate(position, randPos);
+
+	Cube::updateCube(position);
 }
 void PhysicsCleanup() {
 	//TODO
 }
+
